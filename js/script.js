15ed59372f35f64b0ece543c978896639b7edc0b
Djamel Ramdani <enzonpapa@gmail.com>
2021-10-30 21:10:51 -0500

    set up variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var towns = [];
+
+var cityEl = document.querySelector("#town-search");
+var thecity = document.querySelector("#cities");
+var weathernow = document.querySelector("#weather-now");
+var searchOfCity = document.querySelector("#city-search");
+var futurWeather = document.querySelector("#future");
+var theContainerEl = document.querySelector("#next-days");
+var pastSearchingEl = document.querySelector("#prev-search");
