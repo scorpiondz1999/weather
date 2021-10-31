e3f1ae9473f8f3372f68ebdaf9d6c1ba7a3fd13d
Djamel Ramdani <enzonpapa@gmail.com>
2021-10-30 23:51:39 -0500

    setup temperature wind and humidity

--- js/script.js
+++ js/script.js
@@ -60,3 +60,73 @@ var getWeatherDisp = function (dayweath, searchTheCity) {
     `https://openweathermap.org/img/wn/${dayweath.weather[0].icon}@2x.png`
   );
   searchOfCity.appendChild(weatherIcon);
+
+  var Submitform = function (event) {
+    event.preventDefault();
+    var city = thecity.value.trim();
+    if (city) {
+      theWeather(city);
+      get5Day(city);
+      towns.unshift({ city });
+      thecity.value = "";
+    } else {
+      alert("Enter city name ");
+    }
+    savemySearch();
+    pastSearching(city);
+  };
+  
+  var savemySearch = function () {
+    console.log(towns);
+    localStorage.setItem("cities", JSON.stringify(towns));
+  };
+  
+  var theWeather = function (town) {
+    var apiKey = "844421298d794574c100e3409cee0499";
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${town}&units=imperial&appid=${apiKey}`;
+  
+    fetch(apiURL).then(function (response) {
+      response.json().then(function (data) {
+        console.log(data);
+        getWeatherDisp(data, town);
+      });
+    });
+  };
+  
+  var getWeatherDisp = function (dayweath, searchTheCity) {
+    //clear old content
+    weathernow.textContent = "";
+    searchOfCity.textContent = searchTheCity;
+  
+    //console.log(weather);
+  
+    //create date element
+    var sameDate = document.createElement("span");
+    sameDate.textContent =
+      " (" + moment(dayweath.dt.value).format("MMM D, YYYY") + ") ";
+    searchOfCity.appendChild(sameDate);
+  
+    //create an image element
+    var weatherIcon = document.createElement("img");
+    weatherIcon.setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${dayweath.weather[0].icon}@2x.png`
+    );
+    searchOfCity.appendChild(weatherIcon);
+  
+    //wind
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind Speed: " + dayweath.wind.speed + " MPH";
+    windSpeedEl.classList = "list-group-item";
+    //temperature
+    var temp = document.createElement("span");
+    temp.textContent = "Temperature: " + dayweath.main.temp + " °F";
+    temp.classList = "list-group-item";
+  
+    //Humidity
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + dayweath.main.humidity + " %";
+    humidityEl.classList = "list-group-item";
+  
+    
+  
